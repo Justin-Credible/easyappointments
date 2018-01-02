2e792fc6a22d211aa847f5a78a54f602a309da93
Justin Unterreiner <justin.unterreiner@gmail.com>
2018-01-01 22:18:38 -0800

    Read e-mail configuration from environment variables

--- src/application/config/email.php
+++ src/application/config/email.php
@@ -5,10 +5,11 @@
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments'; 
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['protocol'] = 'smtp'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = ''; 
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
+// HACK: JGU: Read e-mail configuration from Azure environment variables.
+$config['smtp_host'] = getenv('email_smtp_host');
+$config['smtp_user'] = getenv('email_smtp_user');
+$config['smtp_pass'] = getenv('email_smtp_pass');
+$config['smtp_crypto'] = getenv('email_smtp_crypto'); // 'ssl' or 'tls'
+$config['smtp_port'] = getenv('email_smtp_port');
